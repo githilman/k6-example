fc3bae126602a7dd6c10e1731d42eb1df50e1f94
githilman <hilman@arc.capital>
2023-04-17 21:57:38 +0700

    test-action

--- test.js
+++ test.js
@@ -2,14 +2,14 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 export let options = {
   stages: [
-    { duration: '10s', target: 20 },
-    { duration: '1m10s', target: 10 },
+    { duration: '10s', target: 50 },
+    { duration: '1m10s', target: 100 },
+    { duration: '30s', target: 50 },
     { duration: '10s', target: 0 },
   ],
 };
 
 export default function () {
-  http.get('http://test.k6.io');
+  http.get('https://tweet-app-tester-performance-test-aijk54j36a-et.a.run.app'); // Ganti dengan cloudrun URL
   sleep(1);
 }
-
